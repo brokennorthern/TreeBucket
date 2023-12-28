ffe40ffcceb5fd3dc5b0ce5c001761552346fb22
Mark Leighton <markconnorleighton@gmail.com>
2023-12-28 16:57:55 +0000

    updated to clean

--- TreeBucket/Program.cs
+++ TreeBucket/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TreeBucket
@@ -13,59 +14,18 @@ namespace TreeBucket
         public static void Main(string[] args)
         {
             var input = File.ReadAllText(@"C:\Users\Mark\source\repos\TreeBucket\TreeBucket\input.txt");
-            //var calibrations = input
-            //    .TrimEnd()
-            //    .Split('\n')
-            //    .
-            //    .ToList();
-
-            string[] strings = input.Split('\n');
-            string[] RefinementNumberOne = new String[strings.Length];
-            int[] RefinementNumberTwo = new int[strings.Length];
+            string[] strings = Regex.Replace(input, @"[A-Za-z]+", "").Split('\n');
             int sum = 0;
+
             for (int i = 0; i < strings.Length; i++)
             {
-                for (int j = 0; j < strings[i].Length; j++)
-                {
-                    if (Char.IsDigit(strings[i][j]))
-                    {
-                        RefinementNumberOne[i] += (strings[i][j]);
-                        //Console.WriteLine(RefinementNumberOne[i]);
-                    }
-                }
-            }
-            for (int i = 0; i < RefinementNumberOne.Length; i++)
-            {
-                if (RefinementNumberOne[i].Length > 0)
-                {
-                    if (RefinementNumberOne[i].Length > 2)
-                    {
-                        string temp = "";
-                        temp += RefinementNumberOne[i][0];
-                        temp += RefinementNumberOne[i][RefinementNumberOne[i].Length - 1];
-                        //Console.WriteLine(temp);
-                        RefinementNumberOne[i] = temp;
-                    }
-                    if (RefinementNumberOne[i].Length < 2)
-                    {
-                        string temp = "";
-                        temp += RefinementNumberOne[i][0];
-                        temp += RefinementNumberOne[i][0];
-                        //Console.WriteLine(temp);
-                        RefinementNumberOne[i] = temp;
-                    }
-                }
-
-                RefinementNumberTwo[i] = Int32.Parse(RefinementNumberOne[i]);
-
-                Console.WriteLine(RefinementNumberTwo[i]);
+                strings[i] = strings[i].Trim();
+                string output = "";
+                output += strings[i][0];
+                output += strings[i][^1];
+                sum += Int32.Parse(output);
             }
 
-            for ( int i = 0;i < RefinementNumberTwo.Length; i++)
-            {
-                sum += RefinementNumberTwo[i];
-                Console.WriteLine($"{i} {sum}");
-            }
             Console.WriteLine(sum);
         }
     }
